bc3b3bf88763cd8293e235836cc53369798171c8
hasan-rifat <developerhr000@gmail.com>
2023-06-21 10:58:18 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# practice-mongoose
+# practice-mongoose
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const port: number = 5000;
+
+// database
+
+async function DB(): Promise<void> {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/prentice-mongoose");
+
+    console.log("connected to database successfully");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(`error connecting to database: ${error}`);
+  }
+}
+
+DB();
